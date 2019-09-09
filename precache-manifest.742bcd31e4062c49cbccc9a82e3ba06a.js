4a019d2aa063fbc979c4cf41f7544a8db8e23490
Praveesh <praveesh91@gmail.com>
2019-09-09 13:53:41 +0530

    Updates

---
+++ precache-manifest.742bcd31e4062c49cbccc9a82e3ba06a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beecdd8de1b686aa937dba1212589635",
+    "url": "/praveesh91/reactProfileCard/index.html"
+  },
+  {
+    "revision": "f856b301d3e36d8f1811",
+    "url": "/praveesh91/reactProfileCard/static/css/main.37fb9f4d.chunk.css"
+  },
+  {
+    "revision": "ed5cf338212fcfba0cad",
+    "url": "/praveesh91/reactProfileCard/static/js/2.ef744a19.chunk.js"
+  },
+  {
+    "revision": "f856b301d3e36d8f1811",
+    "url": "/praveesh91/reactProfileCard/static/js/main.485eecba.chunk.js"
+  },
+  {
+    "revision": "5860cab185cf4d292242",
+    "url": "/praveesh91/reactProfileCard/static/js/runtime~main.bfe76a78.js"
+  }
+]);
